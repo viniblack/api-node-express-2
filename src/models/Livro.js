9d6b85e3442325030680ef0347ac7f3380ba25eb
Vini Black <viniibl4ck@gmail.com>
2023-11-01 15:41:11 -0300

    realizando validação dos campos

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -14,8 +14,21 @@ const livroSchema = new mongoose.Schema({
   editora: {
     type: String,
     required: [true, "A editora é obrigatória"],
+    enum: {
+      values: ["Casa do código", "Alura"],
+      message: "A editora {VALUE} não é um valor permitido",
+    },
   },
-  numeroPaginas: { type: Number },
+  numeroPaginas: {
+    type: Number,
+    validate: {
+      validator: (valor) => {
+        return valor >= 10 && valor <= 5000;
+      },
+      message:
+        "O número de páginas deve estar entre 10 e 5000. Valor fornecido {VALUE}",
+    },
+  },
 });
 
 const livros = mongoose.model("livros", livroSchema);
